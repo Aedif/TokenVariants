8f6896944e0e4f6c3d17500ed3b7e0229f63bb72
Aedif <vaitkus.martynas@gmail.com>
2023-05-13 10:59:01 +0100

    making libwrapper a dependency

--- scripts/wrappers/wrappers.js
+++ scripts/wrappers/wrappers.js
@@ -1,10 +1,10 @@
-import { libWrapper } from '../libWrapper/shim.js';
 import { registerEffectIconWrappers } from './effectIconWrappers.js';
 import { registerUserMappingWrappers } from './userMappingWrappers.js';
 
 export const REGISTERED_WRAPPERS = {};
 
 export function registerWrapper(feature_id, name, fn, method = 'WRAPPER') {
+  if (typeof libWrapper !== 'function') return;
   if (!(feature_id in REGISTERED_WRAPPERS)) REGISTERED_WRAPPERS[feature_id] = {};
   if (name in REGISTERED_WRAPPERS[feature_id]) return;
 
@@ -12,6 +12,7 @@ export function registerWrapper(feature_id, name, fn, method = 'WRAPPER') {
 }
 
 export function unregisterWrapper(feature_id, name) {
+  if (typeof libWrapper !== 'function') return;
   if (feature_id in REGISTERED_WRAPPERS && name in REGISTERED_WRAPPERS[feature_id]) {
     libWrapper.unregister('token-variants', REGISTERED_WRAPPERS[feature_id][name]);
     delete REGISTERED_WRAPPERS[feature_id][name];
