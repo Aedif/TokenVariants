922a527c5fb6b8ea65fa9b77b525bd04362919df
aedif <vaitkus.martynas@gmail.com>
2026-01-26 10:27:46 +0000

    removing jquery for tva button inserts

--- token-variants/scripts/hooks/artSelectButtonHooks.js
+++ token-variants/scripts/hooks/artSelectButtonHooks.js
@@ -10,18 +10,13 @@ export function registerArtSelectButtonHooks() {
   // Insert right-click listeners to open up ArtSelect forms from various contexts
   if (TVA_CONFIG.permissions.portrait_right_click[game.user.role]) {
     registerHook(feature_id, 'renderActorSheet', _modActorSheet);
-    registerHook(feature_id, 'renderItemSheet', _modItemSheet);
+    registerHook(feature_id, 'renderItemSheet5e', _modItemSheet);
     registerHook(feature_id, 'renderItemActionSheet', _modItemSheet);
     registerHook(feature_id, 'renderJournalSheet', _modJournalSheet);
-    registerHook(feature_id, 'renderRollTableConfig', _modRollTableSheet);
   } else {
-    [
-      'renderActorSheet',
-      'renderItemSheet',
-      'renderItemActionSheet',
-      'renderJournalSheet',
-      'renderRollTableConfig',
-    ].forEach((name) => unregisterHook(feature_id, name));
+    ['renderActorSheet', 'renderItemSheet5e', 'renderItemActionSheet', 'renderJournalSheet'].forEach((name) =>
+      unregisterHook(feature_id, name),
+    );
   }
 
   // Insert buttons
@@ -48,7 +43,7 @@ export function registerArtSelectButtonHooks() {
 }
 
 function _modTokenConfig(config, html) {
-  insertArtSelectButton($(html), 'texture.src', {
+  insertArtSelectButton(html, 'texture.src', {
     search: config.document.name,
     searchType: SEARCH_TYPE.TOKEN,
   });
@@ -100,67 +95,46 @@ function _modActiveEffectConfig(effectConfig, html) {
     searchType: TVA_CONFIG.customImageCategories.includes('Active Effect') ? 'Active Effect' : SEARCH_TYPE.ITEM,
   });
   if (!inserted) {
-    const img = $(html).find('.effect-icon');
-    img.on('contextmenu', () => {
+    const img = html.querySelector('[data-edit="img"]');
+    img?.addEventListener('contextmenu', () => {
       showArtSelect(effectConfig.document?.name ?? 'Active Effect', {
         searchType: SEARCH_TYPE.ITEM,
-        callback: (imgSrc) => img.attr('src', imgSrc),
+        callback: (imgSrc) => img.setAttribute('src', imgSrc),
       });
     });
   }
 }
 
 function _modItemSheet(itemSheet, html, options) {
-  $(html)
-    .find('img.profile, .profile-img, [data-edit="img"]')
-    .on('contextmenu', () => {
-      const item = itemSheet.document;
-      if (!item) return;
-      showArtSelect(item.name, {
-        searchType: SEARCH_TYPE.ITEM,
-        callback: (imgSrc) => item.update({ img: imgSrc }),
-      });
+  html.querySelector('[data-action="editImage"]')?.addEventListener('contextmenu', () => {
+    const item = itemSheet.document;
+    if (!item) return;
+    showArtSelect(item.name, {
+      searchType: SEARCH_TYPE.ITEM,
+      callback: (imgSrc) => item.update({ img: imgSrc }),
     });
+  });
 }
 
 function _modMacroConfig(macroConfig, html, options) {
-  const img = $(html).find('.sheet-header > img');
-  img.on('contextmenu', () => {
+  const img = html.querySelector('.sheet-header > img');
+  img?.addEventListener('contextmenu', () => {
     showArtSelect(macroConfig.document?.name ?? 'Macro', {
       searchType: SEARCH_TYPE.MACRO,
-      callback: (imgSrc) => img.attr('src', imgSrc),
+      callback: (imgSrc) => img.setAttribute('src', imgSrc),
     });
   });
 }
 
 function _modJournalSheet(journalSheet, html, options) {
-  $(html)
-    .find('.header-button.entry-image')
-    .on('contextmenu', () => {
-      const journal = journalSheet.document;
-      if (!journal) return;
-      showArtSelect(journal.name, {
-        searchType: SEARCH_TYPE.JOURNAL,
-        callback: (imgSrc) => journal.update({ img: imgSrc }),
-      });
+  html.querySelector('.header-button.entry-image')?.addEventListener('contextmenu', () => {
+    const journal = journalSheet.document;
+    if (!journal) return;
+    showArtSelect(journal.name, {
+      searchType: SEARCH_TYPE.JOURNAL,
+      callback: (imgSrc) => journal.update({ img: imgSrc }),
     });
-}
-
-function _modRollTableSheet(sheet, html) {
-  $(html)
-    .find('.result-image')
-    .on('contextmenu', (event) => {
-      const table = sheet.document;
-      if (!table) return;
-      const img = $(event.target).closest('.result-image').find('img');
-      showArtSelect(table.name, {
-        searchType: TVA_CONFIG.customImageCategories.includes('RollTable') ? 'RollTable' : SEARCH_TYPE.ITEM,
-        callback: (imgSrc) => {
-          img.attr('src', imgSrc);
-          sheet._onSubmit(event);
-        },
-      });
-    });
+  });
 }
 
 /**
