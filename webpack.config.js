b4cd837d518a748166cdb60f6fce463ee8478744
aedif <vaitkus.martynas@gmail.com>
2026-01-26 09:30:25 +0000

    packer build modes / slight restructure

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,29 @@
 const path = require('path');
 const TerserPlugin = require('terser-webpack-plugin');
 
-module.exports = {
-  entry: './token-variants/token-variants.mjs',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'token-variants'),
-    publicPath: 'modules/token-variants/',
-  },
-  optimization: {
-    minimizer: [
-      new TerserPlugin({
-        terserOptions: {
-          keep_classnames: true,
-          keep_fnames: false,
-        },
-      }),
-    ],
-  },
-  mode: 'production',
-  watch: true,
-  devtool: 'source-map',
+module.exports = (env) => {
+  return {
+    entry: './token-variants/token-variants.mjs',
+    output: {
+      filename: 'token-variants.js',
+      path: path.resolve(__dirname, 'token-variants/bundle'),
+      publicPath: 'modules/bundle/token-variants/',
+    },
+    optimization: {
+      minimizer: [
+        new TerserPlugin({
+          terserOptions: {
+            keep_classnames: true,
+            keep_fnames: false,
+          },
+        }),
+      ],
+    },
+    mode: 'production',
+    watch: env?.mode === 'watch' ? true : false,
+    watchOptions: {
+      ignored: ['**/node_modules/', '**/scripts/libs/'],
+    },
+    devtool: 'source-map',
+  };
 };
